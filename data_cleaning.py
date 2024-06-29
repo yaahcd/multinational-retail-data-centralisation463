995e660f041f61aef82d59860e6e2dded4acfed0
Yara <cremaschi.yara@gmail.com>
2024-06-29 11:56:05 +0100

    adds a few changes to cleaning method

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,6 +1,6 @@
-import pandas as pd
-import numpy as np
-from dateutil.parser import parse
+import pandas as pd 
+import numpy as np 
+
 
 class DataCleaning():
 
@@ -8,16 +8,18 @@ class DataCleaning():
         #null values -- inplace modifies current df
         data.fillna(np.nan, inplace=True)
         #error with dates
-        date_format = "%Y%m%d"
-        data['date_of_birth'] = data['date_of_birth'].apply(parse)
-        data['join_date'] = data['join_date'].apply(parse)
-        data['date_of_birth'] = pd.to_datetime(data['date_of_birth'], format=date_format, infer_datetime_format=True, errors='coerce')
-        data['join_date'] = pd.to_datetime(data['join_date'], format=date_format, infer_datetime_format=True, errors='coerce')
+        # data['date_of_birth'].to_string()
+        # data['join_date'].to_string()
+        data['date_of_birth'] = pd.to_datetime(data['date_of_birth'],  errors='coerce')
+        data['join_date'] = pd.to_datetime(data['join_date'],  errors='coerce')
         #incorrect typed values
-        data.str.lower()
+        data['phone_number'].to_string()
+        data['email_address'].to_string()
+        data['email_address'].str.lower()
         data = data.drop_duplicates(subset=['email_address'])
         data['phone_number'] = data['phone_number'].str.replace(".","")
         data['phone_number'] = data['phone_number'].str.replace(" ","")
         #rows filled with the wrong information
         ##
         return data
+    
