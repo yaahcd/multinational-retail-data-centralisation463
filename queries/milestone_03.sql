304f5e9795763692ee0ee379d75ddc2992790737
Yara <cremaschi.yara@gmail.com>
2024-07-13 13:29:21 +0100

    updates milestone_03 with tasks

--- queries/milestone_03.sql
+++ queries/milestone_03.sql
@@ -1,3 +1,18 @@
+-- 01. Change the data types to correspond to those seen in the table below.
+
+-- +------------------+--------------------+--------------------+
+-- |   orders_table   | current data type  | required data type |
+-- +------------------+--------------------+--------------------+
+-- | date_uuid        | TEXT               | UUID               |
+-- | user_uuid        | TEXT               | UUID               |
+-- | card_number      | TEXT               | VARCHAR(?)         |
+-- | store_code       | TEXT               | VARCHAR(?)         |
+-- | product_code     | TEXT               | VARCHAR(?)         |
+-- | product_quantity | BIGINT             | SMALLINT           |
+-- +------------------+--------------------+--------------------+
+
+-- The ? in VARCHAR should be replaced with an integer representing the maximum length of the values in that column.
+
 SELECT MAX(LENGTH(card_number)) AS max_length
 FROM orders_table;
 
@@ -23,6 +38,19 @@ SET DATA TYPE UUID USING user_uuid::UUID;
 
 -----------------------------------------------------------
 
+-- 02. The column required to be changed in the users table are as follows:
+
+-- +----------------+--------------------+--------------------+
+-- | dim_users      | current data type  | required data type |
+-- +----------------+--------------------+--------------------+
+-- | first_name     | TEXT               | VARCHAR(255)       |
+-- | last_name      | TEXT               | VARCHAR(255)       |
+-- | date_of_birth  | TEXT               | DATE               |
+-- | country_code   | TEXT               | VARCHAR(?)         |
+-- | user_uuid      | TEXT               | UUID               |
+-- | join_date      | TEXT               | DATE               |
+-- +----------------+--------------------+--------------------+
+
 SELECT MAX(LENGTH(country_code)) AS max_length
 FROM dim_users;
 
@@ -35,6 +63,25 @@ ALTER COLUMN join_date SET DATA TYPE DATE USING join_date::DATE,
 ALTER COLUMN user_uuid SET DATA TYPE UUID USING user_uuid::UUID;
 
 --------------------------------------------------------------
+
+-- 03. Set the data types for each column as shown below:
+
+-- +---------------------+-------------------+------------------------+
+-- | store_details_table | current data type |   required data type   |
+-- +---------------------+-------------------+------------------------+
+-- | longitude           | TEXT              | FLOAT                  |
+-- | locality            | TEXT              | VARCHAR(255)           |
+-- | store_code          | TEXT              | VARCHAR(?)             |
+-- | staff_numbers       | TEXT              | SMALLINT               |
+-- | opening_date        | TEXT              | DATE                   |
+-- | store_type          | TEXT              | VARCHAR(255) NULLABLE  |
+-- | latitude            | TEXT              | FLOAT                  |
+-- | country_code        | TEXT              | VARCHAR(?)             |
+-- | continent           | TEXT              | VARCHAR(255)           |
+-- +---------------------+-------------------+------------------------+
+
+-- There is a row that represents the business's website change the location column values from N/A to NULL.
+
 SELECT MAX(LENGTH(country_code)) AS max_length
 FROM dim_store_details;
 
@@ -62,6 +109,42 @@ ALTER COLUMN country_code SET DATA TYPE VARCHAR(2),
 ALTER COLUMN continent SET DATA TYPE VARCHAR(255);
 
 -------------------------------------------------------------------
+
+-- 04. You will need to do some work on the products table before casting the data types correctly.
+
+-- The product_price column has a £ character which you need to remove using SQL.
+
+-- The team that handles the deliveries would like a new human-readable column added for the weight so they can quickly make decisions on delivery weights.
+
+-- Add a new column weight_class which will contain human-readable values based on the weight range of the product.
+
+-- +--------------------------+-------------------+
+-- | weight_class VARCHAR(?)  | weight range(kg)  |
+-- +--------------------------+-------------------+
+-- | Light                    | < 2               |
+-- | Mid_Sized                | >= 2 - < 40       |
+-- | Heavy                    | >= 40 - < 140     |
+-- | Truck_Required           | => 140            |
+-- +----------------------------+-----------------+
+
+-- 05. After all the columns are created and cleaned, change the data types of the products table.
+
+-- You will want to rename the removed column to still_available before changing its data type.
+
+-- Make the changes to the columns to cast them to the following data types:
+
+-- +-----------------+--------------------+--------------------+
+-- |  dim_products   | current data type  | required data type |
+-- +-----------------+--------------------+--------------------+
+-- | product_price   | TEXT               | FLOAT              |
+-- | weight          | TEXT               | FLOAT              |
+-- | EAN             | TEXT               | VARCHAR(?)         |
+-- | product_code    | TEXT               | VARCHAR(?)         |
+-- | date_added      | TEXT               | DATE               |
+-- | uuid            | TEXT               | UUID               |
+-- | still_available | TEXT               | BOOL               |
+-- | weight_class    | TEXT               | VARCHAR(?)         |
+-- +-----------------+--------------------+--------------------+
 
 UPDATE dim_products
 SET product_price = REPLACE(product_price, '£', '')
@@ -105,6 +188,18 @@ ALTER COLUMN weight_class SET DATA TYPE VARCHAR(14);
 
 -----------------------------------------------------------------------
 
+-- 06. Now update the date table with the correct types:
+
+-- +-----------------+-------------------+--------------------+
+-- | dim_date_times  | current data type | required data type |
+-- +-----------------+-------------------+--------------------+
+-- | month           | TEXT              | VARCHAR(?)         |
+-- | year            | TEXT              | VARCHAR(?)         |
+-- | day             | TEXT              | VARCHAR(?)         |
+-- | time_period     | TEXT              | VARCHAR(?)         |
+-- | date_uuid       | TEXT              | UUID               |
+-- +-----------------+-------------------+--------------------+
+
 SELECT MAX(LENGTH(month)) AS max_length
 FROM dim_date_times;
 
@@ -126,6 +221,18 @@ ALTER COLUMN date_uuid SET DATA TYPE UUID USING date_uuid::uuid;
 
 --------------------------------------------------------------------------
 
+-- 07. Now we need to update the last table for the card details.
+
+-- Make the associated changes after finding out what the lengths of each variable should be:
+
+-- +------------------------+-------------------+--------------------+
+-- |    dim_card_details    | current data type | required data type |
+-- +------------------------+-------------------+--------------------+
+-- | card_number            | TEXT              | VARCHAR(?)         |
+-- | expiry_date            | TEXT              | VARCHAR(?)         |
+-- | date_payment_confirmed | TEXT              | DATE               |
+-- +------------------------+-------------------+--------------------+
+
 SELECT MAX(LENGTH(card_number)) AS max_length
 FROM dim_card_details;
 
@@ -143,6 +250,16 @@ ALTER COLUMN date_payment_confirmed SET DATA TYPE DATE;
 
 --------------------------------------------------------------------------
 
+-- 08. Now that the tables have the appropriate data types we can begin adding the primary keys to each of the tables prefixed with dim.
+
+-- Each table will serve the orders_table which will be the single source of truth for our orders.
+
+-- Check the column header of the orders_table you will see all but one of the columns exist in one of our tables prefixed with dim.
+
+-- We need to update the columns in the dim tables with a primary key that matches the same column in the orders_table.
+
+-- Using SQL, update the respective columns as primary key columns.
+
 ALTER TABLE dim_date_times
 ADD PRIMARY KEY (date_uuid);
 
@@ -159,6 +276,12 @@ ALTER TABLE dim_products
 ADD PRIMARY KEY (product_code);
 
 --------------------------------------------------------------------------
+
+-- 09. With the primary keys created in the tables prefixed with dim we can now create the foreign keys in the orders_table to reference the primary keys in the other tables.
+
+-- Use SQL to create those foreign key constraints that reference the primary keys of the other table.
+
+-- This makes the star-based database schema complete.
 
 INSERT INTO dim_card_details (card_number)
 SELECT DISTINCT card_number
